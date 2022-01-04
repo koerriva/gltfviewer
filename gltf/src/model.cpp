38e79a3c55f4a53768012415bf8dc515632b9179
杨玉印 <504097978@qq.com>
2022-01-04 18:55:25 +0800

    Update model.cpp

--- gltf/src/model.cpp
+++ gltf/src/model.cpp
@@ -7,6 +7,7 @@
 #include "glm/ext.hpp"
 #include "glad/glad.h"
 
+#define TINYGLTF_USE_CPP14
 #define TINYGLTF_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
